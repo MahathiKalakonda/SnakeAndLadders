f3783d98264f994b775af4e3061f9d274e7fb32f
MahathiKalakonda <mahathi.kalakonda@thoughtworks.com>
2020-02-04 10:49:16 +0530

    Add GameController class

---
+++ GameController.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+class GameController
+{
+    boolean status=false;
+    int currentPlayer=0,numberOfPlayers;
+    Dice dice=new Dice();
+    Scanner scanner=new Scanner(System.in);
+
+    public void startGame()
+    {
+        System.out.println("Enter number of Players between 2 and 6");
+        numberOfPlayers=scanner.nextInt();
+        Player players[]=new Player[numberOfPlayers];
+        Board board = new Board(numberOfPlayers);
+        takeInputs();
+    }
+
+    public void takeInputs()
+    {
+        while(status==false)
+        {
+            int currentPosition=players[(currentPlayer%numberOfPlayers)+1].position
+            int diceValue=players[(currentPlayer%numberOfPlayers)+1].rollDice(dice);
+            players[(currentPlayer%numberOfPlayers)+1].position=board.updatePosition(currentPosition,diceValue);
+            isGameFinished();
+            currentPlayer++;
+        }
+    }
+
+    public void Display()
+    {
+        for(playerNumber=0;playerNumber<numberOfPlayers;playerNumber++)
+            System.out.println("Position of Player "+  (playerNumber+1)+" is "+players[playerNumber].position);
+    }
+
+    public void isGameFinished()
+    {
+        if(players[(currentPlayer%numberOfPlayers)+1].position==board.boardSize)
+        {
+            System.out.println("Player "+((currentPlayer%numberOfPlayers)+1)+" won the game");
+            status=true;
+        }
+        Display();
+    }
+}
