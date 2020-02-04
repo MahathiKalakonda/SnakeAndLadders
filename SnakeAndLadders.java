79376c1641ed5a1073b47b71983c7b339b778a2b
MahathiKalakonda <mahathi.kalakonda@thoughtworks.com>
2020-02-04 10:15:22 +0530

    Add main class

---
+++ SnakeAndLadders.java
@@ -0,0 +1,8 @@
+public class SnakeAndLadders
+{
+    public static void main(String args[])
+    {
+        GameController gameController=new gameController()
+        gameController.startGame();
+    }
+}
